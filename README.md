c267c485df902811758e49ef5e5b182b79db6a56
gaozhenze <514400504@qq.com>
2023-04-18 14:27:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-webcontainer-api-starter-up7yxj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-webcontainer-api-starter-up7yxj)
---
+++ nodetest/index.js
@@ -0,0 +1,7 @@
+var Git = require('nodegit');
+
+Git.Clone('https://github.com/nodegit/nodegit', 'nodegit').then(function (
+  repository
+) {
+  // Work with the repository object here.
+});
